c5afe6ee0ce6526da2d64874a50370dba457ad2c
Colin Maykish <colin.maykish@keeptruckin.com>
2021-04-01 18:13:54 -0400

    Color interpolation function

--- utils.go
+++ utils.go
@@ -21,3 +21,14 @@ func MapIntToInt(v, aMin, aMax, bMin, bMax int) int {
 
 	return result
 }
+
+func InterpColors(colorA, colorB uint32, hue float64) (uint8, uint8, uint8) {
+	var ra = uint8(colorA & (0xFF << 16) >> 16)
+	var ga = uint8(colorA & (0xFF << 8) >> 8)
+	var ba = uint8(colorA & 0xFF)
+	var rb = uint8(colorB & (0xFF << 16) >> 16)
+	var gb = uint8(colorB & (0xFF << 8) >> 8)
+	var bb = uint8(colorB & 0xFF)
+
+	return uint8(float64(rb-ra)*hue) + ra, uint8(float64(gb-ga)*hue) + ga, uint8(float64(bb-ba)*hue) + ba
+}
